de9650a101a27e94049cd90e2f88e29345e96af4
polubis <polubis@wp.pl>
2023-09-20 16:00:47 +0200

    3. [SETUP] Add option to maintain themes

--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/pages/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
   },
   plugins: [],
 };
