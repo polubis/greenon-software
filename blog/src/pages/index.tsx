3fc3842e4295e5ec7543e0ca9b33fa04ec330554
polubis <polubis@wp.pl>
2023-09-20 15:07:55 +0200

    2. [SETUP] Add tailwind

--- blog/src/pages/index.tsx
+++ blog/src/pages/index.tsx
@@ -2,7 +2,14 @@ import React, { type FC } from "react";
 import type { HeadFC, PageProps } from "gatsby";
 
 const IndexPage: FC<PageProps> = () => {
-  return <main>Home page...</main>;
+  return (
+    <main>
+      <h1 className="text-3xl top-5 font-bold underline">Hello!</h1>
+      <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+        Badge
+      </span>
+    </main>
+  );
 };
 
 export default IndexPage;
